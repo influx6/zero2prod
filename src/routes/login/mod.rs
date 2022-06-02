37431fd8ba45ad2ad2fec94ee641e8a023d7c5b1
Alexander Ewetumo <trinoxf@gmail.com>
2022-06-02 23:50:15 +0800

    UPT: Add hmac verify

--- src/routes/login/mod.rs
+++ src/routes/login/mod.rs
@@ -17,7 +17,49 @@ use crate::authentication::auth::{validate_credentials, AuthError, Credentials};
 use crate::domain::application::HmacSecret;
 use crate::utils::error_helpers::error_chain_fmt;
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: String,
+    tag: String,
+}
+
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
+}
+
+pub async fn login_form(
+    query: Option<web::Query<QueryParams>>,
+    secret: web::Data<HmacSecret>,
+) -> HttpResponse {
+    let error_html = match query {
+        None => "".into(),
+        Some(query) => match query.0.verify(&secret) {
+            Ok(error) => {
+                format!(
+                    "<p><i>{}</i></p>",
+                    htmlescape::encode_minimal(&query.0.error)
+                )
+            }
+            Err(e) => {
+                tracing::warn!(
+                    error.message = &e,
+                    error.cause_chain = ?e,
+                    "Failed to verify query parameters using the HMAC tag"
+                );
+                "".into()
+            }
+        },
+    };
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(include_str!("login.html"))
