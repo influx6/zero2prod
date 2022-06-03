8feaf19fdfad315c8512d5242007640c44d1bc46
Alexander Ewetumo <trinoxf@gmail.com>
2022-06-03 11:59:26 +0800

    UPT: Add hmac verify

--- src/routes/login/mod.rs
+++ src/routes/login/mod.rs
@@ -45,10 +45,7 @@ pub async fn login_form(
         None => "".into(),
         Some(query) => match query.0.verify(&secret) {
             Ok(error) => {
-                format!(
-                    "<p><i>{}</i></p>",
-                    htmlescape::encode_minimal(&query.0.error)
-                )
+                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
             }
             Err(e) => {
                 tracing::warn!(
@@ -62,7 +59,35 @@ pub async fn login_form(
     };
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    {}
+    <form action="/login" method="post">
+        <label>Username
+            <input
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+            >
+        </label>
+        <label>Password
+            <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+            >
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>"#,
+        , error_html))
 }
 
 #[derive(thiserror::Error)]
